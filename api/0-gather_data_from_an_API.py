aeb85be150c1b976a6deed89bd69d9e18ebb49bc
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-07-20 12:44:20 -0500

    doc

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Gather data from an API"""
+""" Write a Python script that, using this REST API,
+for a given employee ID, returns information
+about his/her TODO list progress."""
+
 import urllib.request
 import json
 import sys
