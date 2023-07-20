cc493264341cc8b8b93617f48bd4fc55b633f232
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-07-20 14:35:36 -0500

    1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,40 +1,31 @@
 #!/usr/bin/python3
-"""Script that prints specific information from an API"""
+"""Export data in the CSV format"""
 import csv
 import requests
-import sys
+from sys import argv
+
+
+def get_api():
+    """Gather data from an API"""
+    url = 'https://jsonplaceholder.typicode.com/'
+    uid = argv[1]
+
+    # get a specific user from users in jsonplaceholder
+    usr = requests.get(url + 'users/{}'.format(uid)).json()
+    # makes a query string to get tasks based on user id
+    todo = requests.get(url + 'todos', params={'userId': uid}).json()
+
+    with open('{}.csv'.format(uid), 'w') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for employee in todo:
+            user_id = uid
+            username = usr.get('username')
+            task_comp = employee.get('completed')
+            task_title = employee.get('title')
+
+            emp_record = [user_id, username, task_comp, task_title]
+            writer.writerow(emp_record)
 
 
 if __name__ == '__main__':
-    API_URL = 'https://jsonplaceholder.typicode.com'
-
-    id = sys.argv[1]
-    request = requests.get('{}/users/{}/todos'.format(
-        API_URL, id), params={"_expand": "user"})
-
-    response = request.json()
-
-    completed_tasks = [task for task in response if task['completed']]
-    EMPLOYEE_NAME = response[0]['user']['name']
-    NUMBER_OF_DONE_TASKS = len(completed_tasks)
-    TOTAL_NUMBER_OF_TASKS = len(response)
-
-    print("Employee {} is done with tasks({}/{}):".format(
-        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS
-    ))
-
-    for task in completed_tasks:
-        print("\t {}".format(task['title']
-                             ))
-
-    # Export to CSV
-    csv_filename = "{}.csv".format(id)
-    with open(csv_filename, mode='w', newline='') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
-
-        for task in response:
-            task_completed_status = "True" if task['completed'] else "False"
-            writer.writerow(
-                [id, EMPLOYEE_NAME, task_completed_status, task['title']])
-
-    print("Data exported to {}".format(csv_filename))
+    get_api()
