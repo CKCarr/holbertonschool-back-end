f91a9793ac9539665e64fe31770a229e448ba84c
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-07-20 12:47:03 -0500

    gather data

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,53 +1,25 @@
 #!/usr/bin/python3
-""" Write a Python script that, using this REST API,
-for a given employee ID, returns information
-about his/her TODO list progress."""
-
-import urllib.request
-import json
+"""Script that prints specific information from an API"""
+import requests
 import sys
 
+if __name__ == '__main__':
+    API_URL = 'https://jsonplaceholder.typicode.com'
 
-def get_employee_todo_progress(employee_id):
-    try:
-        # Make a GET request to the API
-        url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/" \
-            f"todos"
-        response = urllib.request.urlopen(url)
-        todos = json.loads(response.read().decode())
+    id = sys.argv[1]
+    request = requests.get('{}/users/{}/todos'.format(
+        API_URL, id), params={"_expand": "user"})
 
-        # Extract employee information
-        employee_name = todos[0]['username']
+    response = request.json()
 
-        # Filter completed tasks
-        completed_tasks = [todo['title'] for todo in todos
-                           if todo['completed']]
-        num_completed_tasks = len(completed_tasks)
-        total_tasks = len(todos)
+    completed_tasks = [task for task in response if task['completed']]
+    EMPLOYEE_NAME = response[0]['user']['name']
+    NUMBER_OF_DONE_TASKS = len(completed_tasks)
+    TOTAL_NUMBER_OF_TASKS = len(response)
 
-        # Print the employee's TODO list progress
-        print(f"Employee {employee_name} is done with tasks(
-            {num_completed_tasks}/{total_tasks}): ")
-        print(f"{employee_name}:")
+    print("Employee {} is done with tasks({}/{}):".format(
+        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS
+    ))
 
-        # Print the title of completed tasks
-        for task in completed_tasks:
-            print(f"\t{task}")
-
-    except urllib.error.HTTPError as e:
-        print(f"Error: {e.code} - {e.reason}")
-
-    except urllib.error.URLError as e:
-        print(f"Error: Failed to connect to the server - {e.reason}")
-
-    except json.JSONDecodeError as e:
-        print("Error: Failed to parse JSON response")
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    for task in completed_tasks:
+        print("\t {}".format(task['title']))
